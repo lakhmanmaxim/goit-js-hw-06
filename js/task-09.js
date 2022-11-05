9fee4e176000f75f5879e77526967b3766c97cca
Maxim Lakhman <lakhmanmaxim@gmail.com>
2022-11-06 01:10:58 +0200

    9 заданий
    9 заданий готово

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,23 @@
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+// console.log(getRandomHexColor());
+
+const buttonChangeColor = document.querySelector(".change-color");
+const spanColorText = document.querySelector(".color");
+const body = document.querySelector("body");
+
+buttonChangeColor.addEventListener("click", onBodyChangeColor);
+
+function onBodyChangeColor(e) {
+  spanColorText.textContent = getRandomHexColor();
+
+  body.style.backgroundColor = getRandomHexColor();
 }
